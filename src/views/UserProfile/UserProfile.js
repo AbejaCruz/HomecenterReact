d8ffdc34d6ed2db402b9b081a9f29f5d5b338897
Diego Cruz <abeja.dact_@hotmail.com>
2020-03-22 21:08:44 -0500

    quitar lo del pro

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -1,92 +1,112 @@
 import React from "react";
-export default function UserProfile() {
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Table from "components/Table/Table.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
+
+const styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0"
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF"
+    }
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  }
+};
+
+const useStyles = makeStyles(styles);
+
+export default function TableList() {
+  const classes = useStyles();
   return (
-    <div>
-      <div className="container">
-        <br />
-        <br />
-        <br />
-        <div className="row">
-          <div className="col-sm">
-            <div className="row cuadro">
-              <div className="col-sm fila1 text-center">
-              
-              </div>
-              <div className="col-sm fila2 text-center">
-                <h1>70</h1>
-                <h4>Auxiliares Activos</h4>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm">
-            <div className="col-sm">
-              <div className="row cuadro">
-                <div className="col-sm fila1 text-center">
-                  
-                </div>
-                <div className="col-sm fila2 text-center">
-                  <h1>80%</h1>
-                  <h4>Cumplimiento horas de entrenamiento</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <br />
-        <br />
-        <br />
-        <div className="row">
-          <div className="col-sm">
-            <div className="row cuadro">
-              <div className="col-sm fila1 text-center">
-                
-              </div>
-              <div className="col-sm fila2 text-center">
-                <table>
-                  <tr>
-                    <th>
-                     
-                    </th>
-                    <th>
-                     
-                    </th>
-                    <th>
-                   
-                    </th>
-                    <th>
-                    
-                    </th>
-                    <th>
-                     
-                    </th>
-                  </tr>
-                  <tr>
-                    <td className="verde">23</td>
-                    <td className="verde">25</td>
-                    <td className="verde">9</td>
-                    <td className="rojo">8</td>
-                    <td className="rojo">5</td>
-                  </tr>
-                </table>
-                <h4>Desempeño Total</h4>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm">
-            <div className="col-sm">
-              <div className="row cuadro">
-                <div className="col-sm fila1 text-center">
-                 
-                </div>
-                <div className="col-sm fila2 text-center">
-                  <h1>21:34</h1>
-                  <h4>Horas Promedio operario por mes</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Simple Table</h4>
+            <p className={classes.cardCategoryWhite}>
+              Here is a subtitle for this table
+            </p>
+          </CardHeader>
+          <CardBody>
+            <Table
+              tableHeaderColor="primary"
+              tableHead={["Name", "Country", "City", "Salary"]}
+              tableData={[
+                ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
+                ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
+                ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
+                ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
+                ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
+                ["Mason Porter", "Chile", "Gloucester", "$78,615"]
+              ]}
+            />
+          </CardBody>
+        </Card>
+      </GridItem>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card plain>
+          <CardHeader plain color="primary">
+            <h4 className={classes.cardTitleWhite}>
+              Table on Plain Background
+            </h4>
+            <p className={classes.cardCategoryWhite}>
+              Here is a subtitle for this table
+            </p>
+          </CardHeader>
+          <CardBody>
+            <Table
+              tableHeaderColor="primary"
+              tableHead={["ID", "Name", "Country", "City", "Salary"]}
+              tableData={[
+                ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
+                ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
+                ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
+                [
+                  "4",
+                  "Philip Chaney",
+                  "$38,735",
+                  "Korea, South",
+                  "Overland Park"
+                ],
+                [
+                  "5",
+                  "Doris Greene",
+                  "$63,542",
+                  "Malawi",
+                  "Feldkirchen in Kärnten"
+                ],
+                ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"]
+              ]}
+            />
+          </CardBody>
+        </Card>
+      </GridItem>
+    </GridContainer>
   );
 }
